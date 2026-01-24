68b04f8a72be850fc36afe92d3c41297aa6ea74f
Osmund Peter <amazino33@gmail.com>
2026-01-24 16:32:59 +0100

    fixed: staff column now shows user name.

--- app/Models/OrderPayment.php
+++ app/Models/OrderPayment.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class OrderPayment extends Model
 {
     protected $guarded = [];
+
+    protected $casts = [
+        'amount' => 'decimal:2',
+        'paid_at' => 'datetime',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
