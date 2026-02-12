b97a0d77878df6571c48e7cf26bc396052dc3a78
Osmund Peter <amazino33@gmail.com>
2026-02-12 06:11:09 +0100

    permissin fix

--- app/Policies/PermissionPolicy.php
+++ app/Policies/PermissionPolicy.php
@@ -15,7 +15,7 @@ class PermissionPolicy
      */
     public function viewAny(User $user): bool
     {
-        return $user->hasRole('super_admin');
+        return $user->can('ViewAny:Permission');
     }
 
     /**
@@ -23,7 +23,7 @@ class PermissionPolicy
      */
     public function view(User $user, Permission $permission): bool
     {
-        return $user->hasRole('super_admin');
+        return $user->can('View:Permission');
     }
 
     /**
@@ -31,7 +31,7 @@ class PermissionPolicy
      */
     public function create(User $user): bool
     {
-        return $user->hasRole('super_admin');
+        return $user->can('Create:Permission');
     }
 
     /**
@@ -39,7 +39,7 @@ class PermissionPolicy
      */
     public function update(User $user, Permission $permission): bool
     {
-        return $user->hasRole('super_admin');
+        return $user->can('Update:Permission');
     }
 
     /**
@@ -47,6 +47,6 @@ class PermissionPolicy
      */
     public function delete(User $user, Permission $permission): bool
     {
-        return $user->hasRole('super_admin');
+        return $user->can('Delete:Permission');
     }
 }
