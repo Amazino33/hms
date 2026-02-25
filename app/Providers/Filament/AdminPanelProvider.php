ae1ad4b54d4b1677aaaa27cf4b2e3d3b765fb68c
Osmund Peter <amazino33@gmail.com>
2026-02-25 20:55:40 +0100

    branding fixes

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -37,7 +37,7 @@ class AdminPanelProvider extends PanelProvider
             ->databaseNotifications()
             ->databaseNotificationsPolling('5s')
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => '#E5353A',
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
